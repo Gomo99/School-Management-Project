f12ed85bf578b95566b3c43ffe5cabdd2dfe9029
Gomo99 <gomolemomogale102@gmail.com>
2026-06-17 02:51:44 +0200

    hih

--- ViewModel/AssignLecturerModulesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolProject.ViewModel
-{
-    public class AssignLecturerModulesViewModel
-    {
-        public int UserID { get; set; }
-        public List<int> SelectedModuleIDs { get; set; } = new List<int>();
-        public DateTime AssignedDate { get; set; } = DateTime.Now;
-    }
-}
